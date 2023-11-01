1780f59d21e42e283b4dbabbdff9343b3d6ba914
Lucia Barrenechea <lbarrenecheac@gmail.com>
2023-11-01 11:48:55 -0600

    Created palindrome function

---
+++ palindrome.h
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#ifndef palindrome_H
+#define palindrome_H
+
+using namespace std;
+
+
+
+
+#endif
