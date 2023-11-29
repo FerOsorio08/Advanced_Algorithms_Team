35e64cb67d33ae0c6748b503790a429110c26b02
Fernanda Osorio <A01026502@tec.mx>
2023-11-28 19:56:25 -0600

    start evidencia 2

---
+++ Evidencia2/evidencia2.cpp
@@ -0,0 +1,14 @@
+// Lucia Barrenechea, Ian Holender, Fernanda Osorio
+// 28 de noviembre del 2023
+// Archivo main para solución en CPP
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+}
